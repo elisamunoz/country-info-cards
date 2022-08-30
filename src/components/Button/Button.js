5e9636dbe89fac65d73567c6fd2170c63de6244c
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-08-30 19:28:39 +0200

    button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import styles from "./Button.module.scss";
 
 const Button = ({ onClick, text }) => (
-  <button classNames={styles.button} onClick={onClick}>
+  <button className={styles.button} onClick={onClick}>
     {text}
   </button>
 );
