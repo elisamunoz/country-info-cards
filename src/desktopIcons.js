28ecfc081474e679e5b095ecb6fd8a1204b3dd9e
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-09-08 22:00:54 +0200

    added object file with desktop icon  data

---
+++ src/desktopIcons.js
@@ -0,0 +1,32 @@
+import myGithub from "./assets/images/icons/myComputer.png";
+import myPortfolio from "./assets/images/icons/folder.png";
+import myLinkedin from "./assets/images/icons/network.png";
+import myCV from "./assets/images/icons/printer.png";
+import myCVPDF from "./assets/myCV.pdf";
+
+export const DESKTOP_ICON_INFO = [
+  {
+    name: "My GitHub",
+    icon: myGithub,
+    link: "https://github.com/elisamunoz",
+    alt: "GitHub link"
+  },
+  {
+    name: "My Portfolio",
+    icon: myPortfolio,
+    link: "https://elisamunoz.github.io/eli-portfolio/",
+    alt: "Link to My Portfolio"
+  },
+  {
+    name: "My Linkedin",
+    icon: myLinkedin,
+    link: "https://www.linkedin.com/feed/",
+    alt: "Link to my Linkedin"
+  },
+  {
+    name: "My CV",
+    icon: myCV,
+    link: myCVPDF,
+    alt: "Link to my CV"
+  }
+];
--- src/ui/components/DesktopIcon/DesktopIcon.js
+++ src/ui/components/DesktopIcon/DesktopIcon.js
@@ -1,9 +1,9 @@
 import styles from "./DesktopIcon.module.scss";
 
-const DesktopIcon = ({ name, icon, link }) => (
+const DesktopIcon = ({ name, icon, link, alt }) => (
   <div className={styles.desktopIcon}>
     <a href={link} target="_blank" rel="noopener noreferrer">
-      <img src={icon} className={styles.icon} />
+      <img alt={alt} src={icon} className={styles.icon} />
       <h3 className={styles.name}>{name}</h3>
     </a>
   </div>
